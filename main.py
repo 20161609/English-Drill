5c063465c68ef6761ccc09d7b1aeecd4a825600f
Hojun An <pockguy96@gmail.com>
2025-10-26 15:23:31 -0400

    feat: add main.

---
+++ main.py
@@ -0,0 +1,23 @@
+from Functions.shell import Shell
+quit_commands = {'q!', 'Q!', 'quit', 'QUIT'}
+def __main__():
+    try:
+        shell = Shell()
+        while True:
+            try:
+                command = input(shell.prompt)
+                if len(command):
+                    if command.strip() in quit_commands:
+                        break
+                    else:
+                        shell.fetch(command.strip())
+            except KeyboardInterrupt:
+                print()
+                continue
+            except Exception as e:
+                print("...[ERROR]:", e)
+                continue
+    except Exception as e:
+        print(e)
+if __name__ == '__main__':
+    __main__()
